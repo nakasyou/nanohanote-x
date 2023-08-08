426ee3173ef8f9a69760fca56272c977c33923d1
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-08 12:14:54 +0900

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ export default () => {
   return <header class="w-full flex drop-shadow-xl top-0 sticky">
     <div>
       <a href="/">
-        <img src="/icon.svg" alt="NanohaNote icon" />
+        <img src="/assets/icon.svg" alt="NanohaNote icon" />
       </a>
     </div>
   </header>
