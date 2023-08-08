134ab8e0f9839d8878feaa4a61fc237c2db2a084
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-08 12:12:10 +0900

    Create Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+export default () => {
+  return <header class="w-full flex drop-shadow-xl top-0 sticky">
+    <div>
+      <a href="/">
+        <img src="/icon.svg" alt="NanohaNote icon" />
+      </a>
+    </div>
+  </header>
+}
