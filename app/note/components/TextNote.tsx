8691ff5eb2ddbc4178f1097430291979c479ab81
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-08 12:37:35 +0900

    Update TextNote.tsx

--- app/note/components/TextNote.tsx
+++ app/note/components/TextNote.tsx
@@ -1,12 +1,12 @@
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
-import { PluginNanoha } from "../utils/tiptap-plugin-nanoha.ts"
+import { TipTapPluginNanoha } from "../utils/tiptap-plugin-nanoha.ts"
 
 export default () => {
   const editor = useEditor({
     extensions: [
       StarterKit,
-      PluginNanoha,
+      TipTapPluginNanoha,
     ],
     content: '<p>Hello World!</p>',
   })
