b5baf194489f9c1ff650c05f52ed20c4b59d891c
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-08 13:47:53 +0900

    Update index.tsx

--- app/note/index.tsx
+++ app/note/index.tsx
@@ -1,8 +1,25 @@
 import TextNote from "./components/TextNote.tsx"
+import {
+  IconEye,
+  IconEdit,
+} from "@tabler/icons-react"
+import { useState } from "react"
+import classnames from "classnames"
 
 export default () => {
+  const [mode, setMode] = useState<"edit" | "view">("edit")
   return <>
     <div>This is app!</div>
     <TextNote />
+    <div class="sticky bottom-0">
+      <div class="flex justify-center items-center">
+        <button onClick={()=>setMode("edit")} class={classnames({ "bg-lime-300": mode === "edit" })}>
+          <IconEdit />
+        </button>
+        <button onClick={()=>setMode("view")} class={classnames({ "bg-lime-300": mode === "edit" })}>
+          <IconEye />
+        </button>
+      </div>
+    </div>
   </>
 }
